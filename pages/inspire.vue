8d62175f8d356253e7712e6c83d678441979ef36
FirdaRullya <rullyafirda@gmail.com>
2020-03-12 09:58:01 +0700

    tokokue

---
+++ pages/inspire.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;Selamat Datang Di Toko Kue NIDA&#8221;
+        <footer>
+          <small>
+            <em>&mdash;created by dini dan firda</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
